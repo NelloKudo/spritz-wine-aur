cf793846f347c7ee8ce27196826e1477bd126b56
NelloKudo <marshnelloosu@gmail.com>
2026-05-03 03:21:48 +0200

    build.sh: Built from latest wine-builder steamrt4 tag.

--- wine_builder.sh
+++ wine_builder.sh
@@ -384,7 +384,7 @@ compiler_setup() {
 
     _native_common_cflags="-static-libgcc"
 
-    export CPPFLAGS="-D_GNU_SOURCE -D_TIME_BITS=64 -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -DNDEBUG -D_NDEBUG"
+    export CPPFLAGS="-D_GNU_SOURCE -D_TIME_BITS=64 -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
     _GCC_FLAGS="${_common_cflags} ${_native_common_cflags} ${CPPFLAGS}"
     _CROSS_FLAGS="${_common_cflags} ${CPPFLAGS}"
     _LD_FLAGS="${_common_cflags} ${_native_common_cflags} ${CPPFLAGS} -Wl,-O1,--sort-common,--as-needed -L/usr/local/x86_64/lib/x86_64-linux-gnu -L/usr/local/lib"
